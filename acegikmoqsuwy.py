e7ee372c31c0f92ed5e473c8dadb488317d7da65
ilnomad <nathanc124@nycstudents.net>
2025-11-10 09:25:53 -0500

    lkjlkjffhj  kh kh hkhhdsgfkjdgfshkfkhjkjh kj hkhg kjhgasdkjhgadkjhgs dfkjhgas dfkhadgs fkjaskjga shjgasdfasdfasdfasdfadsfasdfasdfasdfasdfasfdasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf

--- acegikmoqsuwy.py
+++ acegikmoqsuwy.py
@@ -1,67 +1,22 @@
 import requests
 
 def skyblock(skyblock):
-    response = requests.get(f"https://api.hypixel.net/?ref=freepublicapis.com#tag/SkyBlock/paths/~1v2~1skyblock~1bazaar/get{skyblock()}")
+    response = requests.get(f"https://api.hypixel.net/v2/skyblock/bazaar")
     if response.status_code != 200:
         print("Error fetching data!")
         return None
     
     data = response.json()
-    y=0
-    for h in data["products"]:
-        y+=1
-        for i in data["products"][y]:
-            return{
-                "quick_status": data["products"][y]["quick_status"]
-    }
-{
-    "success": True,
-    "lastUpdated": 1590854517479,
-    "products": {
-        "INK_SACK:3": {
-            "product_id": "INK_SACK:3",
-            "sell_summary": [
-                {
-                    "amount": 20569,
-                    "pricePerUnit": 4.2,
-                    "orders": 1
-                },
-                {
-                    "amount": 140326,
-                    "pricePerUnit": 3.8,
-                    "orders": 2
-                }
-            ],
-            "buy_summary": [
-                {
-                    "amount": 640,
-                    "pricePerUnit": 4.8,
-                    "orders": 1
-                },
-                {
-                    "amount": 640,
-                    "pricePerUnit": 4.9,
-                    "orders": 1
-                },
-                {
-                    "amount": 25957,
-                    "pricePerUnit": 5,
-                    "orders": 3
-                }
-            ],
-            "quick_status": {
-                "productId": "INK_SACK:3",
-                "sellPrice": 4.2,
-                "sellVolume": 409855,
-                "sellMovingWeek": 8301075,
-                "sellOrders": 11,
-                "buyPrice": 4.99260315136572,
-                "buyVolume": 1254854,
-                "buyMovingWeek": 5830656,
-                "buyOrders": 85
-            }
+    print(data)
+    c=0
+    for b in data:
+        c+=1
+        for h in data[c]["products"]:
+            y=0
+            for i in data[c]["products"][y]:
+                return{
+                    "quick_status": data["products"][y]["quick_status"]
         }
-    }
-}
+            y+=1
 
-skyblock=skyblock(363463456)
+skyblock(363463456)
