34a6357dd3dee22730c3fad06ed9946d250cf3ef
ilnomad <nathanc124@nycstudents.net>
2025-11-19 09:25:13 -0500

    jfjfjfjfjfjfjfjfjfjfjfjfjfjfjfjfjfjfjfjfjfjfjfjfjfjfjfjfhjfhjfjfjfjfjfjfj

--- passwordthing.py
+++ passwordthing.py
@@ -1,33 +1,35 @@
-# Word Problem: The School Portal Login System
-# Your school is creating a new online portal for students to sign up for accounts. The login system needs a function that accepts two pieces of information from the user:
-# Their email address
-# Their password
-# Before creating the new account, the function must verify that the email and password follow school rules:
-# The email must be a string and must contain an "@" symbol.
-# The password must also be a string.
-# The password must be at least 8 characters long.
-# The password must include at least one number.
-# The password must include at least one uppercase letter.
-# If ANY of these rules are broken, the function should return an error message explaining what went wrong.
-# If EVERYTHING is good, the function should return a dictionary that represents the newly created user.
-
 def signup(email,password):
-    n=0
-    if "@" not in email:
-        return"where ur @ at"
-    if email!=str:
-        return"ur email aint a string"
+    a=b=c=d=e=f=n=0
+    if "@" in email:
+        a=1
+    if type(email)==str:
+        b=1
     for h in password:
         n+=1
-    if not n>=8:
-        return"ur password isnt long enough"
+    if n>=8:
+        c=1
     for h in password:
-        if not h.isupper():
-            return"u dont got uppercase letter"
-        if not h.isdigit():
-            return"u dont got number"
-    if password!=str:
-        return"ur password aint a string"
-    print("you did it")
+        if h.isupper():
+            d=1
+        if h.isdigit():
+            e=1
+    if type(password)==str:
+        f=1
+    if a==b==c==d==e==f==1:
+        print("you did it")
+    else:
+        if a==0:
+            print("@?")
+        if b==0:
+            print("strng email?")
+        if c==0:
+            print("paswrd not laqueous enof")
+        if d==0:
+            print("uprcse ltr?")
+        if e==0:
+            print("nmbr?")
+        if f==0:
+            print("strng paswrd?")
 
-signup("nathanc124@nycstudents.net","passworD123")
+signup("nathanc124@nycstudents.net","Welcome1")
+
