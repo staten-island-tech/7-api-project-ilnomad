5bef73f440b28613e5975696eab235e1653ea219
ilnomad <nathanc124@nycstudents.net>
2025-12-01 09:25:52 -0500

    dsf

--- bdfhjlnprtvxz.py
+++ bdfhjlnprtvxz.py
@@ -1,28 +1,25 @@
 import tkinter as tk
+from tkinter import ttk
 import requests
-
+a=b=c=d=""
 def puzzle():
     response = requests.get("https://lichess.org/api/puzzle/next")
     if response.status_code != 200:
         print("Error fetching data!")
         return None
     data = response.json()
-    a=data["game"]["perf"]["name"],
-    b=data["game"]["clock"],
-    c=data["puzzle"]["solution"],
+    a=data["game"]["perf"]["name"]
+    b=data["game"]["clock"]
+    c=data["puzzle"]["solution"]
     d=data["game"]["pgn"]
+    a_a=ttk.Label(window,text=f"{a},{b},{c},{d}",font=("Helvetica", 14))
+    a_a.pack()
 
-buttonpressed=False
 window=tk.Tk()
 window.geometry('1280x720+320+180')
-window.title("Puzzles on LiChess")
-button=tk.Button(window,text=f"Retrieve a random puzzle.",command=puzzle)
+window.title('Puzzles on LiChess')
+outputtext=ttk.Label(window,text="LiChess Puzzles",font=("Helvetica", 14))
+outputtext.pack()
+button=tk.Button(window,text='Retrieve a puzzle.',command=puzzle)
 button.pack(padx=10,pady=10)
-outputtext=tk.Label(window,text="0,0,0",font=("Courier New",14),fg="blue",wraplength=10)
-if buttonpressed==True:
-    outputtext.config(text=a,font=("Courier New",14),fg="blue")
-    outputtext.config(text=b,font=("Courier New",14),fg="blue")
-    outputtext.config(text=c,font=("Courier New",14),fg="blue")
-    outputtext.config(text=d,font=("Courier New",14),fg="blue")
 window.mainloop()
-
