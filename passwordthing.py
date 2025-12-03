15b4bd919ca9c6e30f1c8152b293de1011c96771
ilnomad <nathanc124@nycstudents.net>
2025-12-03 09:26:11 -0500

    austenjianguanstejiangunasitnaiuniugenigiuansduignaeiusntaijesn ijgaensg

--- passwordthing.py
+++ passwordthing.py
@@ -1,16 +1,29 @@
-""" The email must be a string and must contain an "@" symbol.
-The password must also be a string.
-The password must be at least 8 characters long.
-The password must include at least one number.
-The password must include at least one uppercase letter. """
-
 def signup(email,password):
-    a=b=False
-    if "@" not in email: return("no @?")
-    if type(email)!=str and type(password)!=str: return("smth not str")
-    if len(password)<8: return("pswrd not laqueous enof")
-    if not any (h.isupper for h in email): return("no capital")
-    if not any (h.isdigit for h in email): return("no #")
-    if a==b==True: return("you did it")
-print(signup("nathanc124@nycstudents.net","Welcome1"))
+    a=b=c=d=e=f=n=0
+    if "@" in email:
+        a=1
+    if type(email)==str:
+        b=1
+    if len(password)>=8:
+        c=1
+    if (h.isupper for h in email): d=1
+    if (h.isdigit for h in email): e=1
+    if type(password)==str:
+        f=1
+    if a==b==c==d==e==f==1:
+        print("you did it")
+    else:
+        if a==0:
+            print("@?")
+        if b==0:
+            print("strng email?")
+        if c==0:
+            print("paswrd not laqueous enof")
+        if d==0:
+            print("uprcse ltr?")
+        if e==0:
+            print("nmbr?")
+        if f==0:
+            print("strng paswrd?")
 
+signup("nathanc124@nycstudents.net","Welcome1")
