aa1da984591cbc396dbb81336048f5bf76f0d114
ilnomad <nathanc124@nycstudents.net>
2025-12-19 09:26:27 -0500

    ads

--- bdfhjlnprtvxz.py
+++ bdfhjlnprtvxz.py
@@ -5,13 +5,14 @@ import chess
 import chess.pgn
 from io import StringIO
 class puzzles:
-    def __init__(self,a,b,c,d,e,f):
-        self.__a=a
-        self.__b=b
-        self.__c=c
-        self.__d=d
-        self.__e=e
-        self.__f=f    
+    def __init__(self,a,b,c,d,e,f,g):
+        self.a=a
+        self.b=b
+        self.c=c
+        self.d=d
+        self.e=e
+        self.f=f
+        self.g=g
     def puzzle():
         response = requests.get("https://lichess.org/api/puzzle/next")
         if response.status_code != 200:
@@ -24,42 +25,41 @@ class puzzles:
         d=data["puzzle"]["id"]
         e=data["puzzle"]["themes"]
         f=data["game"]["pgn"]
-        a_a=tk.Label(window,text=f"{c},{f}",font=("Helvetica",14))
+        g=data["puzzle"]["initialPly"]
+        a_a=tk.Label(window,text=f"{c},{g}",font=("Helvetica",14))
         a_a.pack()
-        puzzle.__init__(a,b,c,d,e,f)
-        puzzle.boardprint()
+        puzzle.__init__(a,b,c,d,e,f,g)
+        puzzle.boardprint(g)
     def movechecker():
-        g=False
-        h=0
-        i=inputentry.get()
-        for j in puzzle.__c:
-            while g==False:
-                if i==j:
-                    k=tk.Label(window,text="You successfully found the move.")
-                    k.place(x=950,y=100)
+        a=False
+        b=0
+        c=inputentry.get()
+        for d in puzzle.c:
+            while a==False:
+                if c==d:
+                    e=tk.Label(window,text="You successfully found the move.")
+                    e.place(x=950,y=100)
                     time.sleep(5)
-                    k.destroy()
-                    h+=1
-                    g=True
+                    e.destroy()
+                    b+=1
+                    a=True
                 else: 
-                    a_b=tk.Label(window,text="You didn't find the move.")
-                    a_b.place(x=950,y=100)
+                    f=tk.Label(window,text="You didn't find the move.")
+                    f.place(x=950,y=100)
                     time.sleep(5)
-                    a_b.destroy()
-    def boardprint(self):
-        pgn=StringIO(puzzle.__f)
-        game=chess.pgn.read_game(pgn)
-        move_number = 8
-        for number, move in enumerate(game.mainline_moves()):
-            board.push(move)
-            if number == move_number:  
+                    f.destroy()
+    def boardprint(self,g):
+        print(self.f)
+        pgn=StringIO(self.f)
+        chessgame=chess.pgn.read_game(pgn)
+        move_number=g
+        for number, move in enumerate(chessgame.mainline_moves()):
+            chess.Board.push(move)
+            if number==move_number:  
                 break
-        fen = board.fen()
-        print(fen)
-        print(game)
-        game=chess.Board()
-        print(game)
-
+        fen=chess.Board.fen()
+        fenlabel=tk.Label(window,text=f"{fen}")
+        fenlabel.place(x=200,y=200)
 window=tk.Tk()
 window.geometry("1280x720+320+180")
 window.title("Puzzles on LiChess")
@@ -71,7 +71,7 @@ movecheckerbutton=tk.Button(window,text='Check your move.',command=puzzles.movec
 movecheckerbutton.place(x=1000,y=150)
 inputentry=tk.Entry(window)
 inputentry.place(x=750,y=200)
-puzzle=puzzles("","",[],"",[],"")
+puzzle=puzzles("","",[],"",[],"","")
 alabel=tk.Label(window,text="What's the next move in this puzzle?",font=("Helvetica",14))
 alabel.place(x=650,y=150)
 window.mainloop()
